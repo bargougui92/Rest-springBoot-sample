3d35c1edfcda66c77905f8235e82dfff944dafd7
bargougui92 <oussama.bargougui92@gmail.com>
2018-03-29 21:47:17 -0400

    enhancing the swagger configs by adding dev contacts and properties description

--- src/main/java/com/Restful/webservices/example01/SwaggerConfig.java
+++ src/main/java/com/Restful/webservices/example01/SwaggerConfig.java
@@ -2,10 +2,14 @@ package com.Restful.webservices.example01;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashSet;
+import java.util.Set;
 
 //Configuration 
 //Enable Swagger
@@ -13,13 +17,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	//Bean -Docket
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
-	}
-	
-	//Swagger2
-	//Allpaths
-	//All apis
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "oussama", "http://facebook.com", "oussama@cakemail.com"
+    );
+
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Api Documentation title",
+            "Api Documentation description here",
+            "1.0",
+            "link",
+            DEFAULT_CONTACT,
+            "Apache 2.0",
+            "apacheURL"
+    );
+
+    //swagger-ui.html see swagger docs of the app
+    //Bean -Docket---- /v2/api-docs
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                ;
+    }
+
+    //Swagger2
+    //Allpaths
+    //All apis
 }
--- src/main/java/com/Restful/webservices/example01/user/User.java
+++ src/main/java/com/Restful/webservices/example01/user/User.java
@@ -6,13 +6,18 @@ import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import javax.xml.crypto.Data;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class User {
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast 2 characters")
+	@ApiModelProperty(notes="Name should have at least 2 characters")
 	private String name;
 	
 	@Past
+	//you can find @ApiModelProperty and all the other swagger annotation in the swagger-annotation jar file
+	@ApiModelProperty(notes="Birth date should be in the past") //this will be shown in description of the property in swaggerFile
 	private java.util.Date birthday;
 	
 	public Integer getId() {
